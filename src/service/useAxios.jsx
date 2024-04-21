a308a816d38144342ac22cf4aae420f7c339c004
Betul <betulkoru06@gmail.com>
2024-04-21 18:10:02 +0300

    baseURL edited

--- src/service/useAxios.jsx
+++ src/service/useAxios.jsx
@@ -5,12 +5,14 @@ const useAxios = () => {
   const { token } = useSelector((state) => state.auth)
 
   const axiosWithToken = axios.create({
-    baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    // baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    baseURL: "/api/v1",
     headers: { Authorization: `Token ${token}` },
   })
 
   const axiosPublic = axios.create({
-    baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    // baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    baseURL: "/api/v1",
   })
   return { axiosWithToken, axiosPublic }
 }
